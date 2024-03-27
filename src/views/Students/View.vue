b9018dd32d3366d7d5e8e5b9a3b57ccbd786a286
darshithv <darshithv3392@gmail.com>
2024-03-27 07:53:45 +0530

    Added navbar and tables in it

---
+++ src/views/Students/View.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          Students
+          <RouterLink to="/students/create" class="btn btn-primary float-end">
+             Add Student
+          </RouterLink>
+        </h4>
+      </div>
+      <div class="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Course</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Created At</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+
+          </tr>
+        </tbody>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'students',
+  data() {
+    return {
+      students: []
+    }
+  },
+  mounted(){
+
+    console.log('i am here')
+  }
+}
+
+</script>
